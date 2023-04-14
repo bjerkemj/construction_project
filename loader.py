8504f7574ec6f8e7c276beca4e0ea56b62811d04
@tinusfa <tinusfa@stud.ntnu.no>
2023-04-14 12:44:42 +0200

    Add real excel file

--- loader.py
+++ loader.py
@@ -6,45 +6,41 @@ from project import Project, Task
 ROOT = ROOT = os.path.dirname(os.path.abspath(__file__))
 
 def loadProjectFromFile(filepath: str) -> None:
-    print(filepath)
     projectName =  os.path.basename(filepath).split('.')[0]
-    print(projectName)
-    if filepath.split('.')[-1] == 'xlsx':
-        df = pd.read_excel(filepath)
-        print(df)
-        print('df finished')
-        print()
-        project = Project(projectName)
-        for index, row in df.iterrows():
-            if pd.isnull(row['Predecessors']):
-                predecessors = []
-            else:
-                predecessorsCodes = row['Predecessors'].split(',')
-                predecessors = [project.getTask(code.strip()) for code in predecessorsCodes]
-            if pd.isnull(row['Durations']):
-                durations = [0,0,0]
-            else:
-                durations = [int(duration.replace('(', '').replace(')', '').strip()) for duration in row['Durations'].split(',')]
-            if pd.isnull(row['Description']):
-                row['Description'] = ''
-            task = Task(type = row[0], code = row['Codes'].strip(), description = row['Description'], durations = durations, predecessors=predecessors)
-            project.addTask(task)
-    elif filepath.split('.')[-1] == 'tsv':
-        
-
-        print('Not excel file')
+    df = pd.read_excel(filepath)
+    print(df)
+    print('df finished')
+    print()
+    df.dropna(axis = 0, how = 'all', inplace = True)
+    print(df)
+    print('df finished')
+    print()
+    project = Project(projectName)
+    if 'Descriptions' in df.columns:
+        df.rename(columns={'Descriptions': 'Description'}, inplace=True)
+    for index, row in df.iterrows():
+        if pd.isnull(row['Predecessors']):
+            predecessors = []
+        else:
+            predecessorsCodes = row['Predecessors'].split(',')
+            predecessors = [project.getTask(code.strip()) for code in predecessorsCodes]
+        if pd.isnull(row['Durations']):
+            durations = [0,0,0]
+        else:
+            durations = [int(duration.replace('(', '').replace(')', '').strip()) for duration in row['Durations'].split(',')]
+        if pd.isnull(row['Description']):
+            row['Description'] = ''
+        task = Task(type = row[0], code = row['Codes'].strip(), description = row['Description'], durations = durations, predecessors=predecessors)
+        project.addTask(task)
 
     project.calculateDates()
     project.calculateCriticalTasks()
-    # print(project.tasks)
-
     print(project)
-
     project.tablePrint()
 
 
 def main():
-    filename = 'Warehouse.xlsx'
+    filename = 'Villa_ekte.xlsx'
     filepath = os.path.join(ROOT, filename)
     loadProjectFromFile(filepath=filepath)
 
