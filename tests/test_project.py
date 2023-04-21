1e26bb9cd8bc5c5ac02715e5c39de9cd8d192927
Johan Bjerkem <bjerkem.j@gmail.com>
2023-04-21 13:36:31 +0200

    add extra test

--- tests/test_project.py
+++ tests/test_project.py
@@ -29,6 +29,43 @@ class TestProject(unittest.TestCase):
         project.addTask(task3)
         retrieved_task = project.getTask("A2")
         self.assertEqual(retrieved_task, task2)
+    
+    def test_calculate_dates(self):
+        project = Project("Test Project")
+        task1 = Task(type="Task", code="A1", description="Task 1", durations=[1, 2, 3])
+        task2 = Task(type="Task", code="A2", description="Task 2", durations=[2, 3, 4], predecessors=[task1])
+        task3 = Task(type="Task", code="A3", description="Task 3", durations=[1, 2, 3], predecessors=[task1])
+        task4 = Task(type="Task", code="A4", description="Task 4", durations=[3, 4, 5], predecessors=[task2, task3])
+
+        project.addTask(task1)
+        project.addTask(task2)
+        project.addTask(task3)
+        project.addTask(task4)
+
+        project.calculateDates(1)
+
+        self.assertEqual(task1.getEarlyStartDate(), 0)
+        self.assertEqual(task1.getLateStartDate(), 0)
+        self.assertEqual(task4.getEarlyCompletionDate(), project.getEarlyProjectDuration())
+
+    def test_add_gate(self):
+        project = Project("Test Project")
+        task1 = Task(type="Task", code="A1", description="Task 1", durations=[1, 2, 3])
+        task2 = Task(type="Task", code="A2", description="Task 2", durations=[2, 3, 4], predecessors=[task1])
+        task3 = Task(type="Task", code="A3", description="Task 3", durations=[1, 2, 3], predecessors=[task1])
+        task4 = Task(type="Task", code="A4", description="Task 4", durations=[3, 4, 5], predecessors=[task2, task3])
+
+        project.addTask(task1)
+        project.addTask(task2)
+        project.addTask(task3)
+        project.addTask(task4)
+
+        project.addGate("G1", "Gate 1", ["A1"])
+        gate = project.getTask("G1")
+
+        self.assertIsNotNone(gate)
+        self.assertEqual(len(gate.getPredecessors()), 1)
+        self.assertEqual(len(gate.getSuccessors()), 2)
 
 class TestTask(unittest.TestCase):
     def test_add_predecessor(self):
@@ -74,8 +111,36 @@ class TestTask(unittest.TestCase):
         task1.addSucessor(task3)
         self.assertEqual(len(task1.successors), 2)
 
-        task1.clearPredecessors()
+        task1.clearSucessors()
         self.assertEqual(len(task1.successors), 0)
+
+    def test_calculate_early_dates_no_predecessors(self):
+        task1 = Task(type="Task", code="A1", description="Task 1", durations=[1, 2, 3])
+        task1.calculateEarlyDates()
+
+        self.assertEqual(task1.getEarlyStartDate(), 0)
+        self.assertEqual(task1.getEarlyCompletionDate(), task1.randomDuration)
+
+    def test_calculate_late_dates_no_successors(self):
+        task1 = Task(type="Task", code="A1", description="Task 1", durations=[1, 2, 3])
+        task1.calculateEarlyDates()
+        task1.calculateLateDates()
+
+        self.assertEqual(task1.getLateStartDate(), task1.getEarlyStartDate())
+        self.assertEqual(task1.getLateCompletionDate(), task1.getEarlyCompletionDate())
+
+    def test_is_critical(self):
+        task1 = Task(type="Task", code="A1", description="Task 1", durations=[1, 2, 3])
+        task2 = Task(type="Task", code="A2", description="Task 2", durations=[2, 3, 4])
+
+        task1.earlyStartDate = 0
+        task1.lateStartDate = 0
+
+        task2.earlyStartDate = 3
+        task2.lateStartDate = 4
+
+        self.assertTrue(task1.isCritical())
+        self.assertFalse(task2.isCritical())
 
 if __name__ == '__main__':
     unittest.main()
